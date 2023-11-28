f4a11834d147981ef6d2b59599f0764db4d30b47
Alejandro Gonzalez Hernandez <agonzalezhernan@uoc.edu>
2023-11-28 08:03:22 +0100

    Agregado el componente de Google Maps y primera configuración de la página de centros de buceo.

--- src/app/Components/Centers/centers-map/centers-map.component.ts
+++ src/app/Components/Centers/centers-map/centers-map.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-centers-map',
   templateUrl: './centers-map.component.html',
-  styleUrls: ['./centers-map.component.scss']
+  styleUrls: ['./centers-map.component.scss'],
 })
 export class CentersMapComponent {
+  ngOnInit(): void {}
+  display: any; // Property to store latitude and longitude data from the map
 
+  mapOptions: google.maps.MapOptions = {
+    center: {
+      // Initial center coordinates for the map
+      lat: 40.416775,
+      lng: -3.70379,
+    },
+    zoom: 5.5, // Initial zoom level for the map
+    disableDefaultUI: true,
+  };
+
+  markerOptions: google.maps.MarkerOptions = {
+    draggable: false,
+  };
+  markerPositions: google.maps.LatLngLiteral[] = [];
+  addMarker(event: google.maps.MapMouseEvent) {
+    if (event.latLng != null) this.markerPositions.push(event.latLng.toJSON());
+  }
+
+  marker1 = { position: { lat: 38.9987208, lng: -77.2538699 } };
+  marker2 = { position: { lat: 39.7, lng: -76.0 } };
+  marker3 = { position: { lat: 37.9, lng: -76.8 } };
+
+  markers = [this.marker1, this.marker2, this.marker3];
+
+  move(event: google.maps.MapMouseEvent) {
+    // Method to handle map click event and update the display property
+    if (event.latLng != null) {
+      this.display = event.latLng.toJSON();
+    }
+  }
 }
--- src/app/Components/Centers/centers-map/centers-map.module.ts
+++ src/app/Components/Centers/centers-map/centers-map.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CentersMapRoutingModule } from './centers-map-routing.module';
 import { CentersMapComponent } from './centers-map.component';
 
+import { GoogleMapsModule } from '@angular/google-maps';
+
 @NgModule({
-  imports: [CommonModule, CentersMapRoutingModule],
+  imports: [CommonModule, CentersMapRoutingModule, GoogleMapsModule],
   declarations: [CentersMapComponent],
 })
 export class CentersMapModule {}
